7410687e13bb06986047a0ffb47c5d712f719d91
110689-张云恒 <zhangyh2@belink.com>
2025-08-27 13:23:17 +0800

    修复分支错误

--- frontend/packages/studio/workspace/entry-adapter/src/pages/falcon/marketCardDetail.tsx
+++ frontend/packages/studio/workspace/entry-adapter/src/pages/falcon/marketCardDetail.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /* eslint-disable @coze-arch/max-line-per-function */
 /* eslint-disable prettier/prettier */
 import { useEffect, useCallback, useState, useRef } from 'react';
@@ -10,6 +11,10 @@ import {
   Spin,
   Toast,
   Space,
+  RadioGroup,
+  Radio,
+  Table,
+  Pagination,
 } from '@coze-arch/coze-design';
 import { getParamsFromQuery } from '../../../../../../arch/bot-utils';
 import { useParams } from 'react-router-dom';
@@ -28,7 +33,7 @@ import {
   HeaderActions,
   type DevelopProps,
 } from '@coze-studio/workspace-base/develop';
-import { IconCozPlus } from '@coze-arch/coze-design/icons';
+import { IconCozPlus, IconCozEmpty } from '@coze-arch/coze-design/icons';
 import { GridList, GridItem } from './components/gridList';
 import placeholderImg from './assets/placeholder.png';
 
@@ -38,79 +43,61 @@ import { aopApi } from '@coze-arch/bot-api';
 
 import styles from './index.module.less';
 
-const cardList = [
-  {
-    bizChannel: '',
-    cardClassId: '4',
-    cardId: '10000176',
-    cardName: '理财产品对比',
-    cardPicUrl: '',
-    cardShelfStatus: '1',
-    cardShelfTime: '',
-    code: 'financialProductsComparison',
-    createUserId: '',
-    createUserName: '',
-    picUrl:
-      '@filestore/dev-public-cbbiz/20250714174158_截屏2025-07-14 17.39.55.png',
-    sassAppId: '100001',
-    sassWorkspaceId: '7533521629687578624',
-  },
-  {
-    bizChannel: '',
-    cardClassId: '4',
-    cardId: '10000173',
-    cardName: '产品解读',
-    cardPicUrl: '',
-    cardShelfStatus: '1',
-    cardShelfTime: '',
-    code: 'productInterpretation',
-    createUserId: '',
-    createUserName: '',
-    picUrl:
-      '@filestore/dev-public-cbbiz/20250714161144_截屏2025-07-14 16.11.36.png',
-    sassAppId: '100001',
-    sassWorkspaceId: '7533521629687578624',
-  },
-  {
-    bizChannel: '',
-    cardClassId: '4',
-    cardId: '10000154',
-    cardName: '理财持仓收益-弹窗',
-    cardPicUrl: '',
-    cardShelfStatus: '1',
-    cardShelfTime: '',
-    code: 'financialProductEarningDialog',
-    createUserId: '',
-    createUserName: '',
-    picUrl: '@filestore/dev-public-cbbiz/20250624104919_12.jpg',
-    sassAppId: '100001',
-    sassWorkspaceId: '7533521629687578624',
-  },
-  {
-    bizChannel: '',
-    cardClassId: '4',
-    cardId: '10000143',
-    cardName: '产品赎回',
-    cardPicUrl: '',
-    cardShelfStatus: '1',
-    cardShelfTime: '',
-    code: 'financialProductRedemption',
-    createUserId: '',
-    createUserName: '',
-    picUrl: '@filestore/dev-public-cbbiz/20250606101850_ic_39566.png',
-    sassAppId: '100001',
-    sassWorkspaceId: '7533521629687578624',
-  },
-];
-
 export const FalconMarketCardDetail = () => {
   const cardId = getParamsFromQuery({ key: 'card_id' });
   const creator = getParamsFromQuery({ key: 'creator' });
   const createTime = getParamsFromQuery({ key: 'createTime' });
+  const previewImg = getParamsFromQuery({ key: 'preview_img' });
   const [cardDetail, setCardDetail] = useState({});
+  const [showType, setShowType] = useState('preview');
+  const [addCardId, setAddCardId] = useState('');
+  const [versionPageNum, setVersionPageNum] = useState(1);
+  const [versionList, setVersionList] = useState([]);
+  const [versionTotal, setVersionTotal] = useState(0);
+  const [cardList, setCardList] = useState([]);
+  const pageSize = 30;
   const navigate = useNavigate();
 
+  const addToMe = useCallback(() => {
+    aopApi
+      .CardMarketAddToMe({
+        cardId: cardId,
+      })
+      .then(res => {
+        Toast.success(I18n.t('Added'));
+        setAddCardId(cardId);
+      })
+      .catch(err => {
+        Toast.error(err.message);
+      });
+  }, [cardId]);
+
   useEffect(() => {
+    aopApi
+      .GetCardMarketVersionList({
+        cardId: cardId,
+        pageNo: versionPageNum,
+        pageSize: pageSize,
+      })
+      .then(res => {
+        const newList = res.body.versionList || [];
+        setVersionList(newList);
+        setVersionTotal(Number(res.body.totalNums));
+      });
+  }, [cardId, versionPageNum]);
+
+  useEffect(() => {
+    aopApi
+      .GetCardMarketList({
+        cardClassId: ~~(Math.random() * 11 + 1),
+        pageNo: 1,
+        pageSize: 4,
+      })
+      .then(res => {
+        const newList = res.body.cardList || [];
+        setCardList(newList);
+      });
+
     aopApi
       .GetCardMarketDetail({
         cardId: cardId,
@@ -124,7 +111,7 @@ export const FalconMarketCardDetail = () => {
   }, [cardId]);
 
   return (
-    <Layout className="!overflow-auto">
+    <div className="mt-[16px] mx-[24px]">
       <Header>
         <HeaderTitle>
           <Button
@@ -138,7 +125,7 @@ export const FalconMarketCardDetail = () => {
       </Header>
       <div className={styles.marketCardDetailContent}>
         <div
-          className="pb-[24px] mx-[20px] flex items-start"
+          className="py-[24px] mx-[20px] flex items-start"
           style={{
             borderBottom:
               '1px solid rgba(var(--coze-stroke-5), var(--coze-stroke-5-alpha))',
@@ -160,12 +147,164 @@ export const FalconMarketCardDetail = () => {
               {cardDetail.code}
             </div>
           </div>
-          <Button size="large" type="primary" icon={<IconCozPlus />}>
+          <Button
+            size="large"
+            type="primary"
+            icon={<IconCozPlus />}
+            onClick={addToMe}
+            disabled={addCardId === cardId}
+          >
             {I18n.t('workspace_card_add_my_workstation')}
           </Button>
         </div>
         <div className="mt-[24px] mx-[20px] flex gap-[24px]">
-          <div className="flex-1">{I18n.t('workspace_card_description')}</div>
+          <div className="flex-1">
+            <RadioGroup
+              type="button"
+              value={showType}
+              onChange={e => {
+                setShowType(e.target.value);
+              }}
+            >
+              <Radio value="preview">概览</Radio>
+              <Radio value="version">版本</Radio>
+            </RadioGroup>
+            {showType === 'preview' && (
+              <div className="mt-[16px]">
+                <div className="text-[20px] font-[600] mb-[12px]">
+                  {I18n.t('workspace_card_preview')}
+                </div>
+                <div className="w-full py-[54px] bg-[#EFF0F4] rounded-[6px]">
+                  <div className="w-full h-[300px]">
+                    <img
+                      src={previewImg}
+                      alt=""
+                      className="block h-[100%] mx-[auto]"
+                    />
+                  </div>
+                </div>
+                <div className="text-[20px] font-[600] mb-[12px] mt-[24px]">
+                  {I18n.t('workspace_card_params')}
+                </div>
+                <div
+                  className="w-full px-[24px] py-[24px] bg-[#fff] rounded-[6px] mb-[24px]"
+                  style={{
+                    border:
+                      '1px solid rgba(var(--coze-stroke-5), var(--coze-stroke-5-alpha))',
+                  }}
+                >
+                  <Table
+                    tableProps={{
+                      columns: [
+                        {
+                          key: '1',
+                          title: '参数',
+                          dataIndex: 'paramName',
+                        },
+                        {
+                          key: '2',
+                          title: '名称',
+                          dataIndex: 'paramDesc',
+                        },
+                        {
+                          key: '3',
+                          title: '类型',
+                          dataIndex: 'paramType',
+                          width: 100,
+                          align: 'center',
+                        },
+                        {
+                          key: '4',
+                          title: '是否必填',
+                          dataIndex: 'isRequired',
+                          width: 100,
+                          align: 'center',
+                          render: (text, record) =>
+                            record.isRequired === '1' ? '是' : '否',
+                        },
+                      ],
+                      className: 'bg-[#fff]',
+                      rowKey: 'paramId',
+                      dataSource: cardDetail.paramList || [],
+                      pagination: false,
+                    }}
+                    empty={
+                      <div className="w-full h-full flex flex-col items-center pt-[20px]">
+                        <IconCozEmpty className="w-[48px] h-[48px] coz-fg-dim" />
+                        <div className="text-[16px] font-[500] leading-[22px] mt-[8px] mb-[16px] coz-fg-primary">
+                          {I18n.t('analytic_query_blank_context')}
+                        </div>
+                      </div>
+                    }
+                  />
+                </div>
+              </div>
+            )}
+            {showType === 'version' && (
+              <div
+                className="w-full px-[24px] pt-[24px] pb-[8px] bg-[#fff] rounded-[6px] mt-[16px] mb-[24px]"
+                style={{
+                  border:
+                    '1px solid rgba(var(--coze-stroke-5), var(--coze-stroke-5-alpha))',
+                }}
+              >
+                <Table
+                  tableProps={{
+                    columns: [
+                      {
+                        key: '1',
+                        title: I18n.t('ocean_deploy_list_pkg_version'),
+                        dataIndex: 'version',
+                      },
+                      {
+                        key: '2',
+                        title: I18n.t('bot_publish_columns_platform'),
+                        dataIndex: 'platformStatus',
+                        align: 'left',
+                        render: (_, record) => {
+                          const platform = JSON.parse(
+                            record.platformStatus || '[]',
+                          );
+                          return platform?.join('、') || '-';
+                        },
+                      },
+                      {
+                        key: '3',
+                        title: I18n.t('PublishedTime'),
+                        dataIndex: 'createTime',
+                        align: 'left',
+                        width: 200,
+                      },
+                    ],
+                    className: 'bg-[#fff]',
+                    rowKey: 'versionId',
+                    dataSource: versionList || [],
+                    pagination: {
+                      total: versionTotal,
+                      currentPage: versionPageNum,
+                      pageSize,
+                      onPageChange: setVersionPageNum,
+                    },
+                  }}
+                  empty={
+                    <div className="w-full h-full flex flex-col items-center pt-[20px]">
+                      <IconCozEmpty className="w-[48px] h-[48px] coz-fg-dim" />
+                      <div className="text-[16px] font-[500] leading-[22px] mt-[8px] mb-[16px] coz-fg-primary">
+                        {I18n.t('analytic_query_blank_context')}
+                      </div>
+                    </div>
+                  }
+                />
+                {/* <Pagination
+                  className={styles['version-pagination']}
+                  total={versionTotal}
+                  pageSize={pageSize}
+                  currentPage={versionPageNum}
+                  onPageChange={setVersionPageNum}
+                /> */}
+              </div>
+            )}
+          </div>
           <div className="w-[276px]">
             <div className="text-[18px] font-[600] mb-[20px]">
               {I18n.t('workspace_card_hot_recommend')}
@@ -175,7 +314,7 @@ export const FalconMarketCardDetail = () => {
                 <GridItem key={item.cardId}>
                   <div
                     className={cls(
-                      'px-[16px] h-full flex flex-col justify-between',
+                      'px-[12px] h-full flex flex-col justify-between',
                     )}
                     onClick={e => {
                       navigate(
@@ -184,6 +323,7 @@ export const FalconMarketCardDetail = () => {
                         }&preview_img=${replaceUrl(item.picUrl)}&creator=${
                           item.createUserName
                         }&createTime=${item.cardShelfTime}`,
+                        { replace: true },
                       );
                     }}
                   >
@@ -220,6 +360,6 @@ export const FalconMarketCardDetail = () => {
           </div>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 };
